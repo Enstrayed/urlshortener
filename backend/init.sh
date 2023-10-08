47d793eb79e3f0a649fd1ee16dc4fe6e4bf21177
Enstrayed <48845980+Enstrayed@users.noreply.github.com>
2023-10-08 11:40:22 -0700

    new way to startup backend docker container

---
+++ backend/init.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+mv authorization.example.json authorization.json
+npm install
+node index.js
--- setup-backend.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-echo "IMPORTANT! You must have node & npm installed on the host first!"
-cd ./backend
-npm install
-mv authorization.example.json authorization.json
-cd ..
-echo "Done; modify docker-compose.yml to your liking then start"
